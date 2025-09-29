9b006347ed2661f4595bd138ef5c47c47ee73b8b
devansh deshmukh <devanshd536@gmail.com>
2025-09-29 11:58:50 +0530

    first code gsap

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,54 @@
-import React from 'react' 
+import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Agence from './pages/Agence'
 import Projects from './pages/Projects'
 import gsap from 'gsap'
+import { useRef } from 'react'
 import { useGSAP } from '@gsap/react'
 
 const App = () => {
 
-  useGSAP(function(){
-    gsap.from('.stair',{
-      height:0,
-      stagger:{
-        amount:-0.3
+  const stairParentRef = useRef(null)
+
+  useGSAP(function () {
+
+    const tl = gsap.timeline()
+
+    tl.to(stairParentRef.current, {
+      display: 'block'
+    })
+    tl.from('.stair', {
+      height: 0,
+      stagger: {
+        amount: -0.25
       }
     })
+    tl.to('.stair', {
+      y: '100%',
+      stagger: {
+        amount: -0.25
+      }
+    })
+    tl.to(stairParentRef.current, {
+      display: 'none'
+    })
+    tl.to('.stair', {
+      y:'0%',
+    })  
   })
 
   return (
-    <div className= "text-white">
+    <div className="text-white">
 
-      <div className='h-screen w-full flex fixed z-20 top-0'>
-        <div className='stair h-full w-1/5 bg-black'></div>
-        <div className='stair h-full w-1/5 bg-black'></div>
-        <div className='stair h-full w-1/5 bg-black'></div>
-        <div className='stair h-full w-1/5 bg-black'></div>
-        <div className='stair h-full w-1/5 bg-black'></div>
+      <div ref={stairParentRef} className='h-screen w-full fixed z-20 top-0'>
+        <div className='h-full w-full flex'>
+          <div className='stair h-full w-1/5 bg-black'></div>
+          <div className='stair h-full w-1/5 bg-black'></div>
+          <div className='stair h-full w-1/5 bg-black'></div>
+          <div className='stair h-full w-1/5 bg-black'></div>
+          <div className='stair h-full w-1/5 bg-black'></div>
+        </div>
       </div>
 
       <Routes>

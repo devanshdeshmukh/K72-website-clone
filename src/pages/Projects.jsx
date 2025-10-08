12adea18c16d1d7db7bd9e5cccde5eede20a1a03
devansh deshmukh <devanshd536@gmail.com>
2025-10-08 21:42:43 +0530

    now the turn of animation

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,20 +1,28 @@
 import React from 'react'
+import ProjectCard from '../components/projects/ProjectCard'
 
 const Projects = () => {
+
+    const projects = [{
+    image1: 'https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960-1280x960.jpg',
+    image2: 'https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---Thumbnail-1280x960.jpg'
+  }, {
+    image1: 'https://k72.ca/uploads/caseStudies/OKA/OKA_thumbnail-1280x960.jpg',
+    image2: 'https://k72.ca/uploads/caseStudies/Opto/thumbnailimage_opto-1280x960.jpg'
+  }, {
+    image1: 'https://k72.ca/uploads/caseStudies/LAMAJEURE_-_Son_sur_mesure/chalaxeur-thumbnail_img-1280x960.jpg',
+    image2: 'https://k72.ca/uploads/caseStudies/SHELTON/thumbnailimage_shelton-1280x960.jpg'
+  }]
+
   return (
     <div>
       <div className=' pt-[45vh]'>
-          <h2 className='font-[font2] text-[13vw] uppercase'>Projets</h2>
+        <h2 className='font-[font2] text-[13vw] uppercase'>Projets</h2>
       </div>
       <div className='-mt-18'>
-        <div className='w-full h-[500px] mb-4 flex gap-4'>
-          <div className='w-1/2 group transition-all relative rounded-none hover:rounded-[70px] overflow-hidden h-full '>
-          <img className='h-full w-full object-cover' src="https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960.jpg?w=1280&h=960&s=b5151821a8c0d9603263d7ec827bee9b" alt="" />
-          <div className='opacity-0 transition-all group-hover:opacity-100 absolute top-0 flex items-center justify-center left-0 h-full w-full bg-black/10'>
-            <h2 className='uppercase text-6xl font-[font1] border-4 pt-2 px-6 text-white border-white rounded-full'>vior le projet</h2>
-          </div>
-          </div>
-        </div>
+        {projects.map(function(elem){
+          return <ProjectCard image1={elem.image1} image2={elem.image2}/>
+        })}
       </div>
     </div>
   )

ffbe7b9a6cbb5a597439b6dc33966b001bc85170
devansh deshmukh <devanshd536@gmail.com>
2025-10-11 10:50:58 +0530

    Aggence respomsive

--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -1,6 +1,6 @@
 // import { useGSAP } from '@gsap/react'
 // import gsap from 'gsap'
-// import { ScrollTrigger } from 'gsap/all'
+// import { ScrollTrigger } from 'gsap/all'  
 // import React, { useRef } from 'react'
 
 // const Agence = () => {
@@ -52,7 +52,7 @@
 //   return (  
 //     <div>
 //       <div className='Section1'>
-//         <div ref={imageDivRef} className='absolute overflow-hidden h-[20vw] rounded-3xl w-[15vw] top-45 left-[30vw] bg-red-500'>
+//         <div ref={imageDivRef} className='absolute overflow-hidden h-[20vw] rounded-3xl w-[15vw] lg:top-45 left-[30vw] bg-red-500'>
 //           <img ref={imageRef} className='h-full object-cover w-full' src="https://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg" alt="" />
 //         </div>
 //         <div className='relative font-[font2]'>
@@ -60,7 +60,7 @@
 //             <h1 className='text-[20vw] text-center uppercase leading-[18vw]'>Soixan7e <br />
 //               Douze</h1>
 //           </div>
-//           <div className='pl-[40%] lg-mt-20 mt-4 p-3'>
+//           <div className='pl-[40%] lg:mt-20 mt-4 p-3'>
 //             <p className='text-3xl '>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Notre curiosité nourrit notre créativité. On reste humbles et on dit non aux gros egos, même le vôtre. Une marque est vivante. Elle a des valeurs, une personnalité, une histoire. Si on oublie ça, on peut faire de bons chiffres à court terme, mais on la tue à long terme. C’est pour ça qu’on s’engage à donner de la perspective, pour bâtir des marques influentes.</p>
 //           </div>
 //         </div>
@@ -71,7 +71,8 @@
 //   )
 // }
 // export default Agence
-// //
+// 
+
 
 
 
@@ -110,10 +111,9 @@ const Agence = () => {
   useGSAP(function () {
 
     gsap.to(imageDivRef.current, {
-      
       scrollTrigger: {
         trigger: imageDivRef.current,
-        markers: true,  
+        // markers: true,
         start: 'top 28%',
         end: 'top -70%',
         pin: true,
@@ -140,8 +140,8 @@ const Agence = () => {
   return (
     <div className='parent'>
       <div id='page1' className='py-1 '>
-        <div ref={imageDivRef} className='absolute overflow-hidden lg:h-[20vw] h-[30vw] lg:rounded-3xl rounded-xl lg:w-[15vw] w-[25vw] lg:top-45 -top-80 lg:left-[30vw] left-[30vw]'>
-          <img ref={imageRef} className='h-full object-cover w-full' src="imageArray[0]" alt="" />
+        <div ref={imageDivRef} className='absolute overflow-hidden lg:h-[20vw] h-[30vw] lg:rounded-3xl rounded-xl lg:w-[15vw] w-[25vw] lg:top-45 -top-40 lg:left-[30vw] left-[30vw]'>
+          <img ref={imageRef} className='h-full object-cover w-full' src="https://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg" alt="" />
         </div>
         <div className='relative font-[font2]'>
           <div className='lg:mt-[55vh] mt-[30vh]'>
@@ -149,7 +149,7 @@ const Agence = () => {
               Douze</h1>
           </div>
           <div className='lg:pl-[40%] lg:mt-20 mt-4 p-3'>
-            <p className='lg:text-5xl text-xl leading-tight'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Notre curiosité nourrit notre créativité. On reste humbles et on dit non aux gros egos, même le vôtre. Une marque est vivante. Elle a des valeurs, une personnalité, une histoire. Si on oublie ça, on peut faire de bons chiffres à court terme, mais on la tue à long terme. C’est pour ça qu’on s’engage à donner de la perspective, pour bâtir des marques influentes.</p>
+            <p className='lg:text-6xl text-xl leading-tight'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Notre curiosité nourrit notre créativité. On reste humbles et on dit non aux gros egos, même le vôtre. Une marque est vivante. Elle a des valeurs, une personnalité, une histoire. Si on oublie ça, on peut faire de bons chiffres à court terme, mais on la tue à long terme. C’est pour ça qu’on s’engage à donner de la perspective, pour bâtir des marques influentes.</p>
           </div>
         </div>
       </div>
@@ -161,4 +161,3 @@ const Agence = () => {
 }
 
 export default Agence
-//4:25
